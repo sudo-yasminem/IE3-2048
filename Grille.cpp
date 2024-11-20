1639c4d35a44ce2c42eb4d3d41d6be39f94d300a
sudo-yasminem <yasminemoukhliss@aol.com>
2024-11-20 17:13:49 +0100

    Affichage grille

--- Grille.cpp
+++ Grille.cpp
@@ -1,5 +1,33 @@
-#include "Grille.h"
 #include <iostream>
+#include "Grille.h"
 
 
+Grille::Grille(){
+    for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                grille[i][j] = 0;
+            }
+    }
+}
 
+void Grille::AffichageGrille() const{
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << grille[i][j] << "  |  ";
+            }
+            std::cout << std::endl;
+    }     
+}
+
+void Grille::ValeurGrille(int i,int j,int val){
+    if(i >= 0 && i < 4 && j >= 0 && j < 4){
+        grille[i][j] = val;
+    }
+    else{
+        std::cout << "Erreur d'index" << std::endl;
+    }
+}
+
+
+    
+   
--- Grille.h
+++ Grille.h
@@ -1,8 +1,12 @@
 #include <iostream>
 
-class Grille{
+class Grille{  
+    private:
+    int grille[4][4];
     public:
-    int grille[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    Grille();
+    void ValeurGrille(int i,int j,int val);
+    void AffichageGrille() const;
     int mv_gauche();
     int mv_droite();
     int mv_haut();
